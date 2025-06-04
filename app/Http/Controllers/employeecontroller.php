ccaf436a27a7cc702474e8c1380e4aa4dd90d8b8
Stuffhappens <shithappen858@gmail.com>
2025-06-04 09:22:14 +0800

    push the controller new

--- app/Http/Controllers/employeecontroller.php
+++ app/Http/Controllers/employeecontroller.php
@@ -15,6 +15,46 @@ class employeecontroller extends Controller
         return view ('employee.index');
     }
 
+    public function create()
+    {
+        $request->validate([
+            'fname'=>'required|string',
+            'lname'=>'required|string',
+            'midname'=>'required|string',
+            'age'=>'required|integer',
+            'address'=>'required|string',
+            'zip'=>'required|integer'
+        ]);
+        return view ('employee.create');
+
+    }
+
+    public function edit(int $id){
+        $employees = employee::find($id);
+        return view ('employee.edit', compact('employees'));
+    }
+
+    public function update(Request $request, int $id){
+
+         $request->validate([
+            'fname'=>'required|string',
+            'lname'=>'required|string',
+            'midname'=>'required|string',
+            'age'=>'required|integer',
+            'address'=>'required|string',
+            'zip'=>'required|integer'
+        ]);
+        employee::findOrFail($id)->update($request->all());
+        return redirect ()->back()->with('status','Employee Updated Successfully!');
+    }
+
+    public function destroy(int $id){
+        $employees = employee::findOrFail($id);
+        $employees->delete();
+        return redirect ()->back()->with('status','Employee Deleted!');
+
+    }
+
     // employee::create($request->all());
     // return view ('employee.create');
     // }
